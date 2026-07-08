09c058428c959acc562319990ba39f19a20a12ff
Wander Plassa <57231153+WanderPlassa@users.noreply.github.com>
2026-07-08 14:46:10 -0300

    Update 02_import_data.R

--- code/02_import_data.R
+++ code/02_import_data.R
@@ -1,7 +1,10 @@
 ################################################################################
-# 02 - Import data
+# Import data
+#
+# This replication package assumes that the researcher has already obtained the
+# original PNV 2012 dataset and stored it as:
+#
+# data/raw/pm3549_tipo1_v1.rds
 ################################################################################
-
-rm(list = ls())
 
 Base_geral <- readRDS("data/raw/pm3549_tipo1_v1.rds")
